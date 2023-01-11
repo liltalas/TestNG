82e5b4f0192b564634703025627d6925389b8b63
liltalas <zamirbekovasyrga@gmail.com>
2023-01-10 21:06:48 -0600

    first testing example

---
+++ src/test/java/com/automation/test/BaseTest.java
@@ -0,0 +1,25 @@
+package com.automation.test;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public abstract class BaseTest {
+
+    // @BeforeMethod - will be printed before each method
+    // @BeforeClass - prints only once before anything
+    // @@BeforeSuite - prints before anything only once
+    // @AfterSuite - only once, after everything
+    // @AfterMethod - After every test / any test method
+
+    @BeforeMethod
+    public void setUp(){
+        System.out.println("Open Browser");
+        System.out.println("Open Database Connection ");
+    }
+
+    @AfterMethod
+    public void cleanUp(){
+        System.out.println("Close Browser");
+        System.out.println("Close DataBase Connection");
+    }
+}
---
+++ src/test/java/com/automation/test/LoginTest.java
@@ -0,0 +1,27 @@
+package com.automation.test;
+
+import org.testng.annotations.*;
+
+public class LoginTest extends BaseTest {
+
+    @Test
+
+    public void verifyLoginSuccessful(){
+        System.out.println("Open Website");
+        System.out.println("Do login");
+        System.out.println("Verify Login Successfully");
+    }
+
+    @Test
+
+    public void secondTest(){
+        System.out.println("Second test");
+    }
+
+    @Test
+
+    public void thirdTest(){
+        System.out.println("Third test");
+    }
+
+}
